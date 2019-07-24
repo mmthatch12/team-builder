8a129865589059995b16ea09f2350b44b8bd7f96
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-07-24 13:35:26 -0600

    working on step 2

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react'
 
 export default function TeamList() {
+    const[ teamMember, setTeamMember ] = useState('')
     return (
+        
         <h1>Team List!</h1>
     )
 }
